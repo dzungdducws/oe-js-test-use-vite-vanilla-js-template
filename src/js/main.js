1ca37b593be365adc217ef847d1192d69de88035
dungnd <dzungdducws@gamil.com>
2024-09-25 15:27:35 +0700

    first add

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+import 'the-new-css-reset/css/reset.css';
+import '../styles/style.css';
+
+// import viteLogo from '../assets/images/vite.svg';
+// import javascriptLogo from '../assets/images/javascript.svg';
+
+const dependencies = [
+  'ESlint',
+  'Prettier',
+  'PostCSS',
+  'PostCSS Nesting',
+  'Autoprefixer',
+  'CSS Nano',
+  'CSS Reset',
+];
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+`;
+
+const setupCounter = (element) => {
+  let counter = 0;
+  const counterElement = element;
+  const setCounter = (count) => {
+    counter = count;
+    counterElement.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener('click', () => setCounter(counter + 1));
+  setCounter(0);
+};
+
+setupCounter(document.querySelector('#counter'));
+
+document.querySelector('.tags').innerHTML = dependencies
+  .map((dependency) => `<p>${dependency}</p>`)
+  .join('');
